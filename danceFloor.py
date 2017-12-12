4bb81e6bbac09d44d929235d7c32786677040079
awhiteman625 <awhiteman625@lsr7.net>
2017-12-12 13:23:46 -0600

    Python Files used to hack minecraft

--- danceFloor.py
+++ danceFloor.py
@@ -1,4 +1,4 @@
-from mcpi.minecraft import Mincraft
+from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 import time
 x, y, z = mc.player.getTilePos()
@@ -12,6 +12,9 @@ mc.setBlocks(FX, FY, FZ, FX+w, FY, FZ+L, B)
 while FX <= x <= FX+w and FZ <= z <= FZ+L:
     if B == 41:
         B = 57
+        mc.setBlocks(FX, FY, FZ, FX+w, FY, FZ+L, B)
+        x, y, z = mc.player.getTilePos()
+        time.sleep(0.5)
     else:
         B = 41
         mc.setBlocks(FX, FY, FZ, FX+w, FY, FZ+L, B)

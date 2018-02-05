6e0dcc6558a971f0a6c63c0f984c1732e680cf5b
akary308 <akary308@lsr7.net>
2018-02-05 13:23:02 -0600

    Python files used to hack minecrafy

---
+++ magic wand.py
@@ -0,0 +1,8 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+import time
+time.sleep(60)
+hits = mc.events.pollBlockHits()
+for hit in hits:
+    x, y, z = hit.pos.x, hit.pos.y, hit.pos.z
+    mc.setBlock(x, y, z, 103)

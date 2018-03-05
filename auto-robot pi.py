1f5ebb5437e6b5dd59a73f53a049ec18619f32c2
akary308 <akary308@lsr7.net>
2018-03-05 13:27:05 -0600

    robot pi

--- auto-robot pi.py
+++ auto-robot pi.py
@@ -2,9 +2,35 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(false)
+GPIO.setwarnings(False)
 
 pT = 17
 pE = 18
 GPIO.setup(pT, GPIO.OUT)
 GPIO.setup(pE, GPIO.IN)
+
+try:
+    while True:
+        GPIO.output(pT, False)
+        time.sleep(0.5)
+        GPIO.output(pT, True)
+        time.sleep(0.00001)
+        GPIO.output(pT, False)
+        startTime = time.time()
+        while GPIO.input(pE) == 0:
+            startTime = time.time()
+        while GPIO.input(pE) == 1:
+            stopTime = time.time()
+            if stopTime - startTime >= 0.04:
+                print("you are to close")
+                stopTime = startTime
+                break
+
+        totalTime = stopTime - startTime
+        dist = totalTime * 34300
+        dist = dist / 2
+        print("Distance: " + str(dist) + " mm")
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+        
